fda78049c3eac21225bc70fda67c61285dbe77f1
Rajiv342005 <anju7807745@gmail.com>
2025-10-17 17:05:07 +0530

    Time: 8 ms (67.37%), Space: 64.8 MB (77.08%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.cpp
@@ -3,6 +3,8 @@ public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
         int count =0;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
                 if(nums1[i]==nums2[j]){
