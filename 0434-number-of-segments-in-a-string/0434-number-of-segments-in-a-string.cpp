5826f90dbfcf0fead9a420ab835bb836b51d1295
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2025-11-10 18:19:32 +0530

    Time: 0 ms (100%), Space: 8.3 MB (19.92%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -3,15 +3,18 @@ public:
     int countSegments(string s) {
         int index=0;
         string word= "";
-        vector<string>ans;
+        // vector<string>ans; 
+        int count =0;
         while(index<s.size()){
             while(index<s.size() && s[index]!=' '){
                 word+=s[index++];
             }
-            if(word!="") ans.push_back(word); 
+            // if(word!="") ans.push_back(word); 
+            if(word!="") count++; 
             word = "";
             index++;
         }
-        return ans.size();
+        // return ans.size();
+        return count;
     }
 };
