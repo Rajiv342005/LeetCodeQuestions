45e3cde34e8ec0af2bd4d4f111c89ab2920921e4
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2025-11-22 17:50:40 +0530

    Time: 0 ms (100%), Space: 9 MB (28.09%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int scoreOfString(string s) {
         int sum=0;
-        int pointer1=0;
         int pointer2=1;
         while(pointer2<s.size()){
-            sum+= abs((s[pointer1++]-'a')-(s[pointer2++]-'a'));
+            sum+= abs((s[pointer2-1]-'a')-(s[pointer2++]-'a'));
         }
         return sum;
     }
