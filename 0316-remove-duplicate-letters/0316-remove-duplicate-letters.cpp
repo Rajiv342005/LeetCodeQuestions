98a9b6ae83e74c776cb59fffae4201c254bd4a41
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2025-12-27 07:01:43 +0530

    Time: 1 ms (32.38%), Space: 9.8 MB (22.89%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+       unordered_map<char,int>freq;
+       vector<bool>visited(26,false);
+       stack<char>st;
+       for(char ch:s){
+        freq[ch]++;
+       }
+       int index=0;
+       while(index<s.size()){
+        freq[s[index]]--;
+        if(st.empty()){
+            st.push(s[index]);
+            visited[s[index]-'a'] = true;
+        }
+        else if(visited[s[index]-'a']) {
+            index++;
+            continue;
+        }
+        else{
+            if(st.top()<s[index] && !visited[s[index]-'a']){
+                st.push(s[index]);
+                visited[s[index]-'a'] = true;
+            }
+            else if(!visited[s[index]-'a']){
+                while(!st.empty()&&st.top()>s[index]&&freq[st.top()]){
+                    visited[st.top()-'a'] = false;
+                    st.pop();
+                }
+                st.push(s[index]);
+                visited[s[index]-'a'] = true;
+            }
+        }
+        index++;
+       }
+       string sf = "";
+       while(!st.empty()){
+        sf+=st.top();
+        st.pop();
+       }
+       reverse(sf.begin(),sf.end());
+       return sf;
+    }
+};
