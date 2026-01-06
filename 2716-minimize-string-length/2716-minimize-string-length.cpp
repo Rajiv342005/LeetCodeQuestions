58f5b85da3d2e0f5216ab03af04af7c318f1116a
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-01-06 17:28:38 +0530

    Time: 14 ms (66.41%), Space: 16.9 MB (60.31%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimizedStringLength(string s) {
+        unordered_set<char>used;
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(used.count(s[i])) continue;
+            else{
+                used.insert(s[i]);
+                count++;
+            }
+        }
+        return count;
+    }
+};
