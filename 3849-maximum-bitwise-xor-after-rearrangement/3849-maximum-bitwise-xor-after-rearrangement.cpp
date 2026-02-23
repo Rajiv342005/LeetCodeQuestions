264cd0cdc02816333ab6d036bfc4291d939e3852
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-02-23 12:44:55 +0530

    Time: 36 ms (87.5%), Space: 73 MB (18.75%) - LeetHub

--- 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.cpp
+++ 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.cpp
@@ -7,18 +7,17 @@ public:
             if(t[i]=='1') count1++;
             else count0++;
         }
-        string s1 = "";
         for(int i=0;i<s.size();i++){
             if(s[i]=='1' && count0){
-                s1+='1';
+                s[i]='1';
                 count0--;
             }
             else if(s[i]=='0' && count1){
-                s1+='1';
+                s[i]='1';
                 count1--;
             }
-            else s1+='0';
+            else s[i]='0';
         }
-        return s1;
+        return s;
     }
 };
