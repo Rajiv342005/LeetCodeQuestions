3b006483834c5a3017ddd3a7a2614aa4948dc200
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-02-26 17:41:53 +0530

    Time: 0 ms (100%), Space: 10 MB (89.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-    int RobberHouse(int index,int n,vector<int>&nums,vector<int>&dp){
-        if(index>=n) return 0;
-        if(dp[index]!=-1) return dp[index];
-        dp[index] = max(nums[index]+RobberHouse(index+2,n,nums,dp),RobberHouse(index+1,n,nums,dp));
-        return dp[index];
-    }
+    // int RobberHouse(int index,int n,vector<int>&nums,vector<int>&dp){
+    //     if(index>=n) return 0;
+    //     if(dp[index]!=-1) return dp[index];
+    //     dp[index] = max(nums[index]+RobberHouse(index+2,n,nums,dp),RobberHouse(index+1,n,nums,dp));
+    //     return dp[index];
+    // }
     int rob(vector<int>& nums) {
-       vector<int>dp(nums.size()+2,-1);
-       return RobberHouse(0,nums.size(),nums,dp); 
+        //MethodNo-1(Bottom-up Approach)
+    //    vector<int>dp(nums.size()+2,-1);
+    //    return RobberHouse(0,nums.size(),nums,dp); 
+       // MethodNo-2(Space Optimization Approach)
+       int firstHouse =0;
+       int secondHouse =0;
+       int final;
+       for(int i=nums.size()-1;i>=0;i--){
+        final = max(nums[i]+secondHouse,firstHouse);
+        secondHouse = firstHouse;
+        firstHouse = final;
+       }
+       return final;
     }
 };
