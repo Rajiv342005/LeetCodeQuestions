fc6cabf7d48c1e4bde76e4fc5692b3e881d47231
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-02-28 14:24:50 +0530

    Time: 27 ms (64.11%), Space: 37.1 MB (24.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int CoinChange(int index,int size,int amount,vector<int>&coin,vector<vector<int>>&dp){
+    int Totalways(int index,int amount,vector<int>&coins,vector<vector<int>>&dp){
         if(amount==0) return 1;
-        else if(amount<0 || index>=size) return 0;
+        if(index==0) return 0;
         if(dp[index][amount]!=-1) return dp[index][amount];
-        dp[index][amount] = CoinChange(index,size,amount-coin[index],coin,dp)+CoinChange(index+1,size,amount,coin,dp);
-        return dp[index][amount];
+        if(amount<coins[index-1]) return dp[index][amount] = Totalways(index-1,amount,coins,dp);
+        return dp[index][amount] = Totalways(index,amount-coins[index-1],coins,dp)+Totalways(index-1,amount,coins,dp);
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));     
-        return CoinChange(0,n,amount,coins,dp);
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        return Totalways(n,amount,coins,dp);   
     }
 };
