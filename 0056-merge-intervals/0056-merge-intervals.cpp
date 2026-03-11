dbe0fc80be043165adfbad499214b6a38e133f58
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-03-12 01:58:22 +0530

    Time: 7 ms (40.15%), Space: 24.3 MB (20.91%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,14 +6,11 @@ public:
         vector<int>prev = intervals[0];
         ans.push_back(prev);
         for(int i=1;i<intervals.size();i++){
-            int first = intervals[i][0];
-            int second = intervals[i][1];
-            if(first>=prev[0] && first<=prev[1]){
-                int max1 = max(prev[1],second);
-                int min1 = prev[0];
+            if(intervals[i][0]>=prev[0] && intervals[i][0]<=prev[1]){
+                int max1 = max(prev[1],intervals[i][1]);
                 ans.pop_back();
-                prev = {min1,max1};
-                ans.push_back({min1,max1});
+                prev = {prev[0],max1};
+                ans.push_back({prev[0],max1});
             }
             else{
                 ans.push_back(intervals[i]);
