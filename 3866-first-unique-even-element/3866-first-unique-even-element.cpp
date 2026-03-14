324b81b280d04910b0339dfaa8087d704f3b4984
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-03-14 22:52:54 +0530

    Time: 0 ms (100%), Space: 28.2 MB (75.64%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.cpp
+++ 3866-first-unique-even-element/3866-first-unique-even-element.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     int firstUniqueEven(vector<int>& nums) {
-        unordered_map<int,pair<int,int>>used;
-        for(int i=nums.size()-1;i>=0;i--){
+        unordered_map<int,int>used;
+        for(int i=0;i<nums.size();i++){
             if(nums[i]%2==0){
-                if(used.count(nums[i])){
-                    used[nums[i]].first = i;
-                    used[nums[i]].second++;
-                }
-                else{
-                    used.insert({nums[i],{i,1}});
-                }
+                used[nums[i]]++;
             }
         }
-        int fue =-1;
-        for(auto &it: used){
-            int ele = it.first;
-            int idx = it.second.first;
-            int count = it.second.second;
-            if(count==1){
-                if(fue==-1) fue = idx;
-                else if(fue>idx)
-                fue = idx;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0 && used[nums[i]]==1){
+                return nums[i];
             }
         }
-        if(fue==-1) return-1;
-        return nums[fue];
+        return -1;
     }
 };
