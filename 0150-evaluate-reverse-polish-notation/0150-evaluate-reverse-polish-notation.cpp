baef36ea44c86b52aea2542f6892254239d046bb
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-04-25 13:57:15 +0530

    Time: 0 ms (100%), Space: 17.2 MB (15.39%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,44 +2,17 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int>st;
-        int index=0;
-        int first=0;
-        int second =0;
-        while(index<tokens.size()){
-            if(tokens[index]=="*"){
-                first  = st.top();
-                st.pop();
-                second = st.top();
-                st.pop();
-                st.push(first*second);
-                index++;
-            }
-            else if(tokens[index]=="+"){
-                first  = st.top();
-                st.pop();
-                second = st.top();
-                st.pop();
-                st.push(first+second);index++;
-            }
-            else if(tokens[index]=="-"){
-                first  = st.top();
-                st.pop();
-                second = st.top();
-                st.pop();
-                st.push(second-first);index++;
-            }
-            else if(tokens[index]=="/"){
-                first  = st.top();
-                st.pop();
-                second = st.top();
-                st.pop();
-                st.push(second/first);index++;
-            }
-            else{
-                int num = stoi(tokens[index]);
-                st.push(num);
-                index++;
-            }
+        for(int i=0;i<tokens.size();i++){
+           if(tokens[i]=="+" || tokens[i]=="-" ||
+           tokens[i]=="*" || tokens[i]=="/"){
+            int num1 = st.top(); st.pop();
+            int num2 = st.top(); st.pop();
+            if(tokens[i]=="+") st.push(num2+num1);
+            else if(tokens[i]=="-") st.push(num2-num1);
+            else if(tokens[i]=="*") st.push(num2*num1);
+            else st.push(num2/num1);
+           }
+           else st.push(stoi(tokens[i]));
         }
         return st.top();
     }
