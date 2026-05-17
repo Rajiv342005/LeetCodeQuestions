d2375f86e25ea1ba203506d70fe706141e2e27ad
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-05-17 16:11:16 +0530

    Time: 0 ms (100%), Space: 15.3 MB (86.51%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        // Bottom UP Approach..
+        // Space Optimization Approach..
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>dp(grid.size()+1,vector<int>(grid[0].size()+1,INT_MAX));
+        vector<int>dp(n+1,INT_MAX);
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 if(i==m-1 && j==n-1){
-                    dp[i][j] = grid[i][j];
+                    dp[j] = grid[i][j];
                     continue;
                 }
-                int minpath = min(dp[i+1][j],dp[i][j+1]);
-                dp[i][j] = minpath+grid[i][j];
+                int minpath = min(dp[j],dp[j+1]);
+                dp[j] = minpath+grid[i][j];
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
