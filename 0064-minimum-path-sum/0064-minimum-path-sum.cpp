490e25562d054ec4504de6402bd68ae7d26f5c22
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-05-17 16:08:50 +0530

    Time: 0 ms (100%), Space: 16.4 MB (38.37%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int MPS(int srow,int scol,vector<vector<int>>&grid,vector<vector<int>>&dp){
-        if(srow>=grid.size() || scol>=grid[0].size()) return INT_MAX;
-        if(srow==grid.size()-1 && scol==grid[0].size()-1) return grid[srow][scol];
-        if(dp[srow][scol]!=-1) return dp[srow][scol];
-
-        int rightmove = MPS(srow+1,scol,grid,dp);
-        int downmove = MPS(srow,scol+1,grid,dp);
-
-        int minpath = min(rightmove,downmove);
-        return dp[srow][scol] =  minpath+grid[srow][scol];
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
-        return MPS(0,0,grid,dp);
+        // Bottom UP Approach..
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>dp(grid.size()+1,vector<int>(grid[0].size()+1,INT_MAX));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1){
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                int minpath = min(dp[i+1][j],dp[i][j+1]);
+                dp[i][j] = minpath+grid[i][j];
+            }
+        }
+        return dp[0][0];
     }
 };
