3528417374f00f9d0cfd09b28b84eb0623b06663
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-05-21 20:08:59 +0530

    Time: 10 ms (71.27%), Space: 16.8 MB (37.32%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int LCS(int pointer1,int pointer2,string &word1,string &word2,vector<vector<int>>&dp){
-        if(pointer1>=word1.size() || pointer2>=word2.size()) return 0;
-
-        if(dp[pointer1][pointer2]!=-1) return dp[pointer1][pointer2];
-
-        if(word1[pointer1]==word2[pointer2]){
-            return dp[pointer1][pointer2] =  1+LCS(pointer1+1,pointer2+1,word1,word2,dp);
-        }
-
-        int len1 = LCS(pointer1+1,pointer2,word1,word2,dp);
-        int len2 = LCS(pointer1,pointer2+1,word1,word2,dp);
-        return dp[pointer1][pointer2] = max(len1,len2);
-    }
     int minDistance(string word1, string word2) {
-        // Traditional Recursion 
+        // Tabulation Method[Bottom up Approach] 
         int m = word1.size();
         int n = word2.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        int lcs = LCS(0,0,word1,word2,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(word1[i]==word2[j]){
+                    dp[i][j] = 1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        int lcs = dp[0][0];
         return m+n - 2*lcs;
     }
 };
