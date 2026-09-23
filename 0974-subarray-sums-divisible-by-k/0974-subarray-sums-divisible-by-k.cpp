68cde9099242cd8e1fb6d95476805160a2cf8c36
Rajiv Ranjan Sharma <anju7807745@gmail.com>
2026-09-23 12:38:38 +0530

    Time: 3 ms (91.1%), Space: 34.5 MB (90.61%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -4,13 +4,13 @@ public:
         int totalCount = 0;
         int currentSum = 0;
         int n = nums.size();
-        unordered_map<int,int>freq;
+        vector<int>freq(k,0);
         freq[0] = 1;
         for(int val: nums){
             currentSum += val;
             currentSum %= k;
             if(currentSum<0) currentSum += k;
-            if(freq.count(currentSum)) totalCount += freq[currentSum];
+            if(freq[currentSum]) totalCount += freq[currentSum];
             freq[currentSum]++; 
         }
         return totalCount;
